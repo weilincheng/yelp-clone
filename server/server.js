e036a767f4d2a81c14339fbbc597eee0a197c731
weilincheng <7236196+weilincheng@users.noreply.github.com>
2021-03-20 15:36:35 +0800

    Implement new route

--- server/server.js
+++ server/server.js
@@ -41,14 +41,21 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
 });
 
 // Create a new restaurant
-app.post("/api/v1/restaurants/", (req, res) => {
+app.post("/api/v1/restaurants/",  async (req, res) => {
     console.log(req.body);
-    res.status(200).json({
-        status: "success",
-        data: {
-            restaurant: "mcdonalds"
-        },
-    });
+    try {
+        const results = await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *", 
+        [req.body.name, req.body.location, req.body.price_range]);
+        console.log(results);
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0],
+            },
+        });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 // Update Restaurants
