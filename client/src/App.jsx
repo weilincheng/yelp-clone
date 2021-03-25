175e2e42bfe2ba86866b145b1ec0a98d351c5e01
weilincheng <7236196+weilincheng@users.noreply.github.com>
2021-03-26 07:51:44 +0800

    Add components for home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import UpdatePage from './routes/UpdatePage';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 const App = () => {
-    return <div>
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/restaurant/:id/update" component={UpdatePage}/>
-                <Route exact path="/restaurant/:id" component={RestaurantDetailPage}/>
-            </Switch>
-        </Router>
-    </div>;
+    return (
+        <div className="container">
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/restaurant/:id/update" component={UpdatePage}/>
+                    <Route exact path="/restaurant/:id" component={RestaurantDetailPage}/>
+                </Switch>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
