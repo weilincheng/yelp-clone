1ea385ecf348094045fbd2d1964e26ae431875cc
weilincheng <7236196+weilincheng@users.noreply.github.com>
2021-03-26 14:04:01 +0800

    Add React context API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,21 @@ import Home from './routes/Home';
 import RestaurantDetailPage from './routes/RestaurantDetailPage';
 import UpdatePage from './routes/UpdatePage';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 
 const App = () => {
     return (
-        <div className="container">
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route exact path="/restaurant/:id/update" component={UpdatePage}/>
-                    <Route exact path="/restaurant/:id" component={RestaurantDetailPage}/>
-                </Switch>
-            </Router>
-        </div>
+        <RestaurantsContextProvider>
+            <div className="container">
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/restaurant/:id/update" component={UpdatePage}/>
+                        <Route exact path="/restaurant/:id" component={RestaurantDetailPage}/>
+                    </Switch>
+                </Router>
+            </div>
+        </RestaurantsContextProvider>
     );
 };
 
